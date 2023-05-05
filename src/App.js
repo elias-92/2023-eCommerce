a01794312229ce6aa2faef20a1aab7ac6e8035ed
elias-92 <martinezelias17@gmail.com>
2023-05-05 13:04:26 -0300

    add actions to reduce and  routes to app component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,40 @@ import CheckoutPage from './Components/CheckoutPage';
 import NavBar from './Components/NavBar';
 import Products from './Components/Products';
 import {Route, Routes} from 'react-router-dom';
+import SignIn from './Components/SignIn';
+import SignUp from './Components/SignUp';
+import {useStateValue} from './stateProvider';
+import {useEffect} from 'react';
+import {getAuth} from 'firebase/auth';
+import {actionTypes} from './reducer';
+import {firebaseApp} from './firebase';
+import Checkout from './Components/CheckoutForm/Checkout';
 
 function App() {
+	const auth = getAuth(firebaseApp);
+
+	const [, dispatch] = useStateValue();
+
+	useEffect(() => {
+		auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				dispatch({
+					type: actionTypes.SET_USER,
+					user: authUser,
+				});
+			}
+		});
+	}, [auth, dispatch]);
+
 	return (
 		<>
 			<NavBar />
 			<Routes>
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
 				<Route path="/" element={<Products />} />
 				<Route path="/checkout-page" element={<CheckoutPage />} />
+				<Route path="/checkout" element={<Checkout />} />
 			</Routes>
 		</>
 	);
--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,19 @@
 export const initialState = {
 	basket: [],
+	user: null,
+	shippingData: {},
 };
 export const actionTypes = {
 	ADD_TO_BASKET: 'ADD_TO_BASKET',
 	REMOVE_ITEM: 'REMOVE_ITEM',
+	SET_USER: 'SET_USER',
+	SIGN_OUT: 'SIGN_OUT',
+	SET_SHIPPING_DATA: 'SET_SHIPPING_DATA',
 };
+export const getBasketTotal = (basket) =>
+	basket?.reduce((acc, item) => item.price + acc, 0);
+
 export const reducer = (state, action) => {
-	console.log('desde reducer:', action);
 	switch (action.type) {
 		case 'ADD_TO_BASKET':
 			return {
@@ -24,6 +31,22 @@ export const reducer = (state, action) => {
 			return {
 				...state,
 				basket: newBasket,
+			};
+		case 'SET_USER':
+			return {
+				...state,
+				user: action.user,
+			};
+		case 'SIGN_OUT':
+			return {
+				...state,
+				basket: action.basket,
+				user: action.user,
+			};
+		case 'SET_SHIPPING_DATA':
+			return {
+				...state,
+				shippingData: action.shippingData,
 			};
 
 		default:
